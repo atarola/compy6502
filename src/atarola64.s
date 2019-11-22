7f9d30fb701fc9148ab37d738eb32e50d761e478
atarola <anthony.tarola@gmail.com>
2019-11-21 23:49:33 -0600

    acia working!

--- src/atarola64.s
+++ src/atarola64.s
@@ -4,20 +4,42 @@
 
 .code
 
+ACIA_DATA = $E000
+ACIA_STATUS = $E001
+ACIA_COMMAND = $E002
+ACIA_CONTROL = $E003
+
 ; app entrypoint
 main:
-    ldx $00
+    lda #%00001011
+    sta ACIA_COMMAND
+    lda #%00011111
+    sta ACIA_CONTROL
 
- @loop:
-    ldx $aa
-    stx $E000
-    nop
+write:
+    ldx #0
 
-    ldx $55
-    stx $6000
-    nop
+next_char:
+wait_txd_empty:
+    lda ACIA_STATUS
+    and #$10
+    beq wait_txd_empty
+    lda text, x
+    beq read
+    sta ACIA_DATA
+    inx
+    jmp next_char
 
-    jmp @loop
+read:
+wait_rxd_full:
+    lda ACIA_STATUS
+    and #$08
+    beq wait_rxd_full
+    lda ACIA_DATA
+    jmp write
+
+text:
+    .byte "Hello World!", $0d, $0a, $00
 
 ; IRQ handler
 on_irq:
