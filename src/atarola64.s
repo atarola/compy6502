525b47e75e28efa14189ba2fb0c91163000e9d14
atarola <anthony.tarola@gmail.com>
2019-11-24 14:30:11 -0600

    ram test

--- src/atarola64.s
+++ src/atarola64.s
@@ -4,42 +4,23 @@
 
 .code
 
-ACIA_DATA = $E000
-ACIA_STATUS = $E001
-ACIA_COMMAND = $E002
-ACIA_CONTROL = $E003
+FIRST = $0300
+SECOND = $0301
 
 ; app entrypoint
 main:
-    lda #%00001011
-    sta ACIA_COMMAND
-    lda #%00011111
-    sta ACIA_CONTROL
-
-write:
-    ldx #0
-
-next_char:
-wait_txd_empty:
-    lda ACIA_STATUS
-    and #$10
-    beq wait_txd_empty
-    lda text, x
-    beq read
-    sta ACIA_DATA
-    inx
-    jmp next_char
-
-read:
-wait_rxd_full:
-    lda ACIA_STATUS
-    and #$08
-    beq wait_rxd_full
-    lda ACIA_DATA
-    jmp write
-
-text:
-    .byte "Hello World!", $0d, $0a, $00
+    ldx #$55
+    stx FIRST
+    ldx #$aa
+    stx SECOND
+ @loop:
+    ldx FIRST
+    stx $9001
+    nop
+    ldx SECOND
+    stx $9001
+    nop
+    jmp @loop
 
 ; IRQ handler
 on_irq:
