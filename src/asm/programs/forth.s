0c6e5179dcd45803918300269bdc0acc2bcd83f2
atarola <anthony.tarola@gmail.com>
2026-05-17 13:13:09 -0500

    changed name and custom word storage to a heap

--- src/asm/programs/forth.s
+++ src/asm/programs/forth.s
@@ -3,7 +3,10 @@
 .include "include/compy6502.inc"
 .include "forth/forth.inc"
 
-.org $1000
+.org $0700
 .include "forth/vm.inc"
 .include "forth/primitives.inc"
 .include "forth/shell.inc"
+
+.org $1000
+.include "forth/program.inc"
